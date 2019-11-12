02bb74578888a2727c3d981a2c014441bf96c976
halmulaiki <56926472+halmulaiki@users.noreply.github.com>
2019-11-11 19:50:28 -0500

    made the footer updated the grid with hover effect

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ window.onscroll =()=> {scrollFunction()}
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("topH1").style.fontSize = "10px"
   } else {
-    document.getElementById("topH1").style.fontSize = "20px"
+    document.getElementById("topH1").style.fontSize = "40px"
   }
 }
 //addeventlistener('scroll', scrollFunction)
+// curser =()=> {
+//   let popup = document.getElementById("grid")
+//   popup.classList.toggle("show")
+// }
